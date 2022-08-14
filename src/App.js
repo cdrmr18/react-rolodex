8ee2f8755ebe35cf42c85ee64b96f52a4580d2fb
Kathy Tavia <kathy.tavia@gmail.com>
2022-08-14 22:59:35 +0200

    added hooks for state & fetching users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const { users, setUsers } = useState([]);
+  const { value, setValue } = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        response.json();
+      })
+      .then((data) => setUsers(data));
+  }, [setUsers]);
+
+  const filteredUsers = users.filter((user) => {
+    return user.toLowerCase().includes(value.toLowerCase());
+  });
+
   return (
     <div className="App">
       <h1>Rolodaex</h1>
